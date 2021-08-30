2274f93fbd5d8ac5705b19451043866b513f35ae
TheMalaysianDude <57954772+TheMalaysianDude@users.noreply.github.com>
2021-08-30 18:30:04 +0800

    minor changes DrawPiston

--- src/af/world/draw/DrawPiston.java
+++ src/af/world/draw/DrawPiston.java
@@ -24,8 +24,8 @@ public class DrawPiston extends DrawBlock{
 		
 		for(int dx = 1; dx <= x; dx++){
 			for(int dy = 1; dy <= y; dy++){
-				float tx = (spacing * dx /2) - (spacing * x / 2);
-				float ty = (spacing * dy /2) - (spacing * y / 2);
+				float tx = ((dx/2) - (x/2)) * spacing;
+				float ty = ((dy/2) - (y/2)) * spacing;
 				
 				Draw.rect(piston, build.x + tx, build.y + ty);
 			}
